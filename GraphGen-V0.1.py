90a9cdc5806dbca4aa59f4bf4d8b26beacee8c2a
S-Azimi <s.azimi@gmail.com>
2024-04-27 18:08:07 +0330

    install libs and start edges

--- GraphGen-V0.1.py
+++ GraphGen-V0.1.py
@@ -1,1 +1,17 @@
-# this is graph generation test by Sasan Azimi using "memgraph"
+# this is graph generation test by Sasan Azimi using "NetworkX"
+
+import networkx as nx  # pip install networkx
+
+import matplotlib.pyplot as plt
+
+G= nx.Graph()
+
+G.add_edge('A','B',weight =13, relation = 'friend')
+G.add_edge('B','C',weight =9, relation = 'family')
+G.add_edge('B','D',weight =7, relation = 'friend')
+G.add_edge('E','B',weight =13, relation = 'friend')
+G.add_edge('E','A',weight =13, relation = 'enemy')
+G.add_edge('F','B',weight =13, relation = 'family')
+G.edges(data=True)
+
+
