04e3f0deb7725c4be615391f2d61446e98c5d5e1
S-Azimi <s.azimi@gmail.com>
2024-05-08 13:29:43 +0330

    last test of graph with 10000 nodes

--- GraphGen-V0.3.py
+++ GraphGen-V0.3.py
@@ -17,7 +17,7 @@ len_string_properties=48
 plt.rcParams["figure.figsize"] = [120, 120]
 edges=[]
 nodeNames=[]
-pathNames= [('A',800), ('B',800),('C',800),('D',800),('E',800),('F',800),('G',800),('H',800)]
+pathNames= [('A',1992), ('B',1200),('C',1200),('D',1200),('E',1200),('F',1200),('G',1200),('H',800)]
 # pathNames= [('A',80), ('B',80),('C',80),('D',80),('E',80),('F',80),('G',80),('H',80)]
 # pathNames= [('A',8), ('B',8),('C',8),('D',8),('E',8),('F',8),('G',8),('H',8)]
 
@@ -90,6 +90,8 @@ nx.write_gml(G, "graph.gml")
 print("\n [  %2f seconds  ]\n" % (time.time() - start_time))
 
 # load a graph from gml file
+
+start_time = time.time()
 print("\n", "load a graph from gml file...")
 H = nx.read_gml('graph.gml')
 print("\n\033[93m", H, "\n\033[0m")
