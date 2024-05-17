653324f18ca9f11e9b530465076865c7fad3f5e3
S-Azimi <s.azimi@gmail.com>
2024-05-17 12:47:08 +0330

    pass self and instance variable

--- cytoscape/tmp.py
+++ cytoscape/tmp.py
@@ -2,17 +2,18 @@ print ("Node class test")
 
 class node:
     n=100  # define a CLASS attribute, not required to set while creating the instance
+    label="node label 1"
     def __init__(self, p,q):
         self.p=p # define an Instance attribute (required to set while creating an instance)
         self.q=q # define an Instance attribute (required to set while creating an instance)
         print (p*q)
         def z():
             print("instance method")
-    def y():
-            print("instance method")
+    def y(self, c):
+            print("instance method", c)
 
-    def x():
-         print(f"this is the node  {self.p} of graph")
+    def x(self):
+         print(f"this is the node {self.n} of graph")
 
 a=node(1,2) # the class attribute is not set so the class value is used
 b=node(11,12)
@@ -25,4 +26,6 @@ print("a",a.n, "b",b.n)  # ->a 100 b 2000
 node.n=53
 
 print(a.n)  # -> 53
+print(a.label)
 a.x()
+a.y(34)
